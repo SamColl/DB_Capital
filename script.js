039296e1d39b1f0afd4744e491dae069a342bae6
Sam Roberts <sam_coll@hotmail.com>
2019-11-15 17:59:55 +0800

    Fixed wave sliders and made client requested change to experience section.

--- script.js
+++ script.js
@@ -2,3 +2,13 @@
 function myFunction() {
   document.getElementById("myLinks").style.display = "block";
 }
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    document.getElementById("header").style.fontSize = "30px";
+  } else {
+    document.getElementById("header").style.fontSize = "90px";
+  }
+}
