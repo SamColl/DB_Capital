6402b698519fd1f86e89d0554993bf75b8a19cde
Sam Roberts <sam_coll@hotmail.com>
2019-11-20 14:11:56 +0800

    changed the hero picture

--- script.js
+++ script.js
@@ -1,14 +1,7 @@
-/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
-function myFunction() {
-  document.getElementById("myLinks").style.display = "block";
-}
 
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-    document.getElementById("header").style.fontSize = "30px";
-  } else {
-    document.getElementById("header").style.fontSize = "90px";
-  }
+$(window).scroll(function() {
+    var scroll = $(window).scrollTop();
+    if (scroll = 100) {
+        $("main-header").addClass("hide");
+    }
 }
