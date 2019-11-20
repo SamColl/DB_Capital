1a3a2cbd44f03d9d69ad3a537d9f4e966a6c2f18
Sam Roberts <sam_coll@hotmail.com>
2019-11-20 18:12:38 +0800

    Added shrinking header with jquery

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 
-$(window).scroll(function() {
-    var scroll = $(window).scrollTop();
-    if (scroll = 100) {
-        $("main-header").addClass("hide");
-    }
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    $(".menulogo-container").hide();
+    $(".header-image").hide();
+    $(".fixed-header").show();
+    $(".hero").css('top', '96px');
+
+  } else {
+    document.getElementById("header").style.fontSize = "90px";
+  }
 }
