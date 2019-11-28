1d6fa0b4e2a932738de3a6ac8e660b34bb04cdff
Sam Roberts <sam_coll@hotmail.com>
2019-11-28 15:32:47 +0800

    removed shrinking header

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-window.onscroll = function() {scrollFunction(x)};
+/*window.onscroll = function() {scrollFunction(x)};
 
 function scrollFunction(x) {
   if ((x.matches) && ((document.body.scrollTop > 60) || (document.documentElement.scrollTop > 60))) {
@@ -18,6 +18,7 @@ function scrollFunction(x) {
 }
 
 }
+*/
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
   document.body.scrollTop = 0; // For Safari
